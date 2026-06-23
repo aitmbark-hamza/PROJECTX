59a6b3ad5fef8a0d45ad9167b55f2445f6f7640d
Hamza Mr <aitmbarkhamza777@gmail.com>
2026-06-23 20:50:40 +0200

    Remove hero watermark

--- tritor/src/components/sections/Hero/Hero.jsx
+++ tritor/src/components/sections/Hero/Hero.jsx
@@ -111,10 +111,6 @@ export default function Hero({ onBookingClick }) {
         </div>
       </div>
 
-      <div className={styles.logoWatermark}>
-        <img src="/images/LOGO.webp" alt="" aria-hidden="true" />
-      </div>
-
       <div className={styles.content}>
         <motion.div
           className={styles.inner}
